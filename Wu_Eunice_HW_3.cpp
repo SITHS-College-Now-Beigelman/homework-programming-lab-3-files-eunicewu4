add0c6e742a71cd8b3859a9af73c33226b16bc5e
eunicewu4 <eunicew6@nycstudents.net>
2024-10-16 18:14:44 -0400

    draft

--- Wu_Eunice_HW_3.cpp
+++ Wu_Eunice_HW_3.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -16,26 +17,40 @@ int main () {
     char type;
     double amt;
 
-    log >> start; // stores the starting balance
+    log >> start; // stores the starting balance, prints this line below
+    cout << "Starting Balance: $" << fixed << setprecision(2) << showpoint << start << endl; // also formats numbers to look like money for all numbers
+    cout << endl;
 
-    for (int i=0; i< 7; ++i) { //loop that keeps storing type of transation and amt, then prints type and balance subtracted/added
-        log >> type >> amt;
-        switch (type) {
+    for (int i=0; i< 7; ++i) { //repeats 7 times. a loop that store type of transation and amt, then uses to prints type and balance subtracted or added
+        log >> type >> amt; //first reads and stores type and amt from log
+        switch (type) { // based if its a withdrawl or transaction, different steps will follow
             case 'W':
-                start = start - amt;
-                cout << "Withdrawl, Balance: " << start;
-                break;
+                start = start - amt; // adds amt to balance
+                cout << "Withdrawal, Balance: " << start <<endl; //prints
+                break; //stop this case and repeat loop if goal not met
             case 'D':
-                start = start + amt;
-                cout << "Deposit, Balance: " << start;
+                start = start + amt; 
+                cout << "Deposit, Balance: " << start << endl;
                 break;
                 }       
         }
-    cout << "Ending balance: " << start; // prints ending balance
+    cout << endl;
+    cout << "Ending balance: $" << start; // prints ending balance
 
     log.close (); //closes the input file
 
-
-
         return 0;
     }
+
+/*Starting Balance: $5000.00
+
+Withdrawal, Balance: 4800.00
+Withdrawal, Balance: 4000.00
+Withdrawal, Balance: 3960.00
+Deposit, Balance: 4050.00
+Deposit, Balance: 4150.00
+Deposit, Balance: 5150.00
+Withdrawal, Balance: 5130.00
+
+Ending balance: $5130.00
+*/
--- Wu_Eunice_HW_3pt2.cpp
+++ Wu_Eunice_HW_3pt2.cpp
@@ -9,46 +9,86 @@
 using namespace std;
 
 int main () {
-    ifstream log; // declares input file name
+// declares input/output file name
+    ifstream log; 
     ofstream statement;
 
-    log.open ("log.txt"); // opens input file name
+// opens input/output file name
+    log.open ("log.txt"); 
     statement.open ("statement.txt");
 
-    double start; // declare variables so values from input value can be used
+// declare variables so values from input value can be used. set some to zero to avoid random numbers.
+    double start; 
     char type;
     double amt;
-    double withdrawl;
+    int withdrawl =0;
     double withdrawlsum;
-    double deposit;
+    int deposit =0;
     double depositsum;
 
-    cout << fixed << setprecision(2);
+// stores and prints the starting balance from log to statement
+    log >> start; 
+    statement << "Starting Balance: $" << fixed << setprecision(2) << showpoint << start << endl; //makes numbers appear like money for the rest
+    statement << endl;
+    
+// uses output manipulators to print and format sections into "Type           Amount         Balance" into statement
+    statement << left << setw(15) << "Type" << setw(15) << "Amount"<< "Balance" << endl;
 
-    log >> start; // stores the starting balance
-
-    for (int i=0; i< 7; ++i) { //loop that keeps storing type of transation and amt, then prints type and balance subtracted/added
+// keeps going until the end loop. keeps formating info from log into statement
+    while (!log.eof()) {
         log >> type >> amt;
         switch (type) {
-            case 'W':
-                withdrawl = withdrawl + 1;
-                withdrawlsum = withdrawlsum + amt;
-                start = start - amt;
-                statement << "Withdrawl, Balance: " << start;
+           case 'W':
+                withdrawl = withdrawl + 1; // calculates the times of withdrawls to later print at end
+                withdrawlsum = withdrawlsum + amt; // sums up withdrawl amount
+                start = start - amt; // calculates balance, later prints it out 
+                statement << setfill('.') << setw(15) << type << setw(15) << amt << start << endl; // setfills dots instead of spaces
                 break;
             case 'D':
-                deposit = deposit + 1;
+                deposit = deposit + 1; // same as withdrawl, but stored into respective variables
                 depositsum = depositsum + amt;
                 start = start + amt;
-                statement << "Deposit, Balance: " << start;
+                statement << setfill('.') << setw(15) << type << setw(15) << amt << start << endl;
                 break;
-                }       
-        }
-    statement << "Ending balance: " << start; // prints ending balance
-    statement << "Number of Withdrawls: " << withdrawl << "Number of Deposit: " << deposit; // prints ending balance
-    statement << "Sum of Withdrawls: " << withdrawlsum << "Sum of Deposit: " << depositsum;
+         }  
+    }
+    // after reaches the end of log, will move on to print summary
+    statement << endl;
+    statement << "Ending balance: $" << start << endl; // prints ending balance
+    statement << "Sum of Withdrawls: $" << withdrawlsum << endl; // sum of withdrawl
+    statement << "Sum of Deposit: $" << depositsum << endl; // sum of deposit
+    statement << "Withdrawals Times: " << withdrawl << endl; //withdrawl times 
+    statement << "Deposit Times: " << deposit << endl; // deposit times
 
-    log.close (); //closes the input file
+    //closes the input/otput file
+    log.close (); 
     statement.close ();
+        
         return 0;
     }
+    /*
+    Starting Balance: $5000.00
+
+Type           Amount         Balance
+W..............200.00.........4800.00
+W..............800.00.........4000.00
+W..............40.00..........3960.00
+D..............90.00..........4050.00
+D..............100.00.........4150.00
+D..............1000.00........5150.00
+W..............20.00..........5130.00
+W..............800.00.........4330.00
+W..............40.00..........4290.00
+D..............90.00..........4380.00
+D..............100.00.........4480.00
+D..............1000.00........5480.00
+W..............20.00..........5460.00
+
+Ending balance: $5460.00
+Sum of Withdrawls: $1920.00
+Sum of Deposit: $2380.00
+Withdrawals Times: 7
+Deposit Times: 6
+
+      
+    */
